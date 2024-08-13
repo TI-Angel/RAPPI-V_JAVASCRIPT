a3ea90417eeb9cd4a17b2ea0d98c69727b69a731
TI-Angel <macaicedo03@gmail.com>
2024-08-12 23:32:23 -0500

    Sprint 3 COMPLETADO

--- javascript/home.js
+++ javascript/home.js
@@ -194,25 +194,77 @@ const data = {
       },
     ],
   };
-  let numeroElementos = data.events.length;
 
-  console.log(data.events.length);
+let container = document.getElementById("contenedor");
+const mostrarEventos = (listaEventos) => {
+  
+  container.innerHTML = "";
 
-  let container = document.getElementById("contenedor");
-
-  for (let i=0; i < numeroElementos; i++){
-    let nuevaTarjeta = document.createElement("div");
+  listaEventos.forEach((evento) => {
+    const nuevaTarjeta = document.createElement("div");
     nuevaTarjeta.className = "col-12 col-sm-5 col-md-4 col-lg-3 card p-0"
     nuevaTarjeta.innerHTML = `
-      <img src="${data.events[i].image}" class="card-img-top" alt="${data.events[i].name}">
+      <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
     <div class="card-body">
-      <h5 class="card-title">${data.events[i].name}</h5>
-      <p class="card-text">${data.events[i].description}</p>
+      <h5 class="card-title">${evento.name}</h5>
+      <p class="card-text">${evento.description}</p>
       <div class="card-bottom d-flex justify-content-between">
-        <p>Price: $${data.events[i].price}</p>
-        <a href="#" class="btn btn-primary">Details</a>
+        <p>Price: $${evento.price}</p>
+        <a href="./pages/detail.html?id=${evento._id}" class="btn btn-primary">Details</a>
       </div>
     </div>`;
+  container.appendChild(nuevaTarjeta);
+  });
+};
 
-    container.appendChild(nuevaTarjeta);
+
+//Generar Checkbox
+let contaierCheckbox = document.getElementById('checkbox');
+let categorias = [];
+
+data.events.forEach(event => {
+  if(!categorias.includes(event.category)){
+    categorias.push(event.category);
   }
+});
+console.log(categorias);
+
+for (let i=0; i < categorias.length; i++){
+  let checkbox = document.createElement('div');
+  checkbox.className = 'form-check form-check-inline';
+  checkbox.innerHTML = `
+  <input class="form-check-input" type="checkbox" id="checkbox" value="${categorias[i]}">
+  <label class="form-check-label" for="checkbox">${categorias[i]}</label>`
+
+  contaierCheckbox.appendChild(checkbox);
+}
+
+//Buscador
+let buscarInput = document.getElementById("buscar");
+let mensajeNoEncontrado = document.getElementById("noResultado");
+const buscador = () => {
+  let buscarEventos = buscarInput.value.toLowerCase();
+  let filtrarEventos = data.events.filter((evento) => evento.name.toLocaleLowerCase().startsWith(buscarEventos));
+  
+  if(filtrarEventos.length === 0){
+    mensajeNoEncontrado.style.display = "block";
+  }
+  else {
+  mensajeNoEncontrado.style.display = "none";
+  }
+mostrarEventos(filtrarEventos);
+}
+buscarInput.addEventListener("input", buscador);
+
+//Checkbox
+document.getElementById("checkbox").addEventListener('change',(e)=>{
+  let chekeado = document.querySelectorAll('input[type=checkbox]:checked');
+  let eventosChekeados = Array.from(chekeado).map(checkbox => checkbox.value);
+  console.log(eventosChekeados);
+  
+  let filtrarEventosCheckbox = data.events.filter(events => 
+    eventosChekeados.length === 0 ||  eventosChekeados.includes(events.category));
+  mostrarEventos(filtrarEventosCheckbox);
+});
+
+mostrarEventos(data.events);
